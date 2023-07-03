47ea1824918824cd4bd06fb0250dd3087c82f42b
DESKTOP-EPBA0RO\Lenovo <sourabh.sharma0185@gmail.com>
2023-07-03 18:07:51 +0530

    update html files.

--- src/main/java/com/example/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -1,9 +1,16 @@
 package com.example.kafka;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.kafka.cmd.CommandPromptApplication;
 
@@ -25,4 +32,23 @@ public class KafkaApplication {
 //    public LagAnalyzerService getFunnyBean() {
 //        return new LagAnalyzerService();
 //    }
+	
+	@Controller 
+	public class HomeContoller {
+		 // inject via application.properties
+	    @Value("${welcome.message}")
+	    private String message;
+
+	    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+
+	    @GetMapping("/")
+	    public String main(Model model) {
+	        model.addAttribute("message", message);
+	        model.addAttribute("tasks", tasks);
+
+	        return "welcome"; //view
+	    }
+	}
+	
+	
 }
